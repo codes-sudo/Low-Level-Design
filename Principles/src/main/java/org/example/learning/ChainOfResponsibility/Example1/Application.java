97f34796584860ed9cc87d00afe2af7a7fa4f787
jibranshawl <jibranshawl@gmail.com>
2024-09-27 08:39:46 +0530

    first commit

---
+++ Principles/src/main/java/org/example/learning/ChainOfResponsibility/Example1/Application.java
@@ -0,0 +1,7 @@
+package org.example.learning.ChainOfResponsibility.Example1;
+
+public class Application {
+    public static void main(String[] args) {
+        System.out.println("jibran");
+    }
+}
